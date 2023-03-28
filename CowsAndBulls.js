a9bd12e302d6ed0151fe4e6d4459562b74d35f29
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-03-28 18:47:17 +0530

    cows and bulls

--- CowsAndBulls.js
+++ CowsAndBulls.js
@@ -80,3 +80,39 @@ var getHint = function (secret, guess) {
   let ans = bulls + "A" + cows + "B";
   return ans;
 };
+
+// another approach is to use an array of 10 elements filled with "0 "that dynamically changes as the value frequency of secret and cows is mapped in real time ;
+
+//time complexity : O(1)
+
+function cows(secret, guess) {
+  let bulls = 0;
+
+  let cows = 0;
+
+  let arr = Array(10).fill(0);
+
+  for (let i = 0; i < secret.length; i++) {
+    let gu = +guess[i];
+
+    let sec = +secret[i];
+
+    if (sec === gu) {
+      bulls++;
+      continue;
+    }
+
+    if (arr[sec] < 0) {
+      cows++;
+    }
+
+    if (arr[gu] > 0) {
+      cows++;
+    }
+
+    arr[sec]++;
+    arr[gu]--;
+  }
+
+  return `${bulls}A${cows}B`;
+}
