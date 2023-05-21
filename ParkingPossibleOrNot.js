95450da20d78a4a56d52cec63329a2bf9e376ad2
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-05-22 04:23:24 +0530

    Parking possible on not

---
+++ ParkingPossibleOrNot.js
@@ -0,0 +1,19 @@
+function ParkingPossible(k, ar, d) {
+  let n = ar.length;
+  let count = 1;
+  let ans = 1;
+  let i = 1;
+  let j = 0;
+
+  while (i < n && j < n) {
+    if (ar[i] <= d[j]) {
+      count++;
+      i++;
+    } else {
+      count--;
+      j++;
+    }
+    ans = Math.max(ans, count);
+  }
+  ans <= k ? console.log("Possible") : console.log("Not possible");
+}
