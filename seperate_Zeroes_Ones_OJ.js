7f69ca8e4546aef822ddc9ae8f32f68ec18348f0
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-06-28 03:23:06 +0530

    seperate zeroes and ones

---
+++ seperate_Zeroes_Ones_OJ.js
@@ -0,0 +1,44 @@
+let arr = [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+function ZeroOne(arr) {
+  let countZero = 0;
+  let countOne = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      countZero++;
+    } else {
+      countOne++;
+    }
+  }
+
+  for (let i = 0; i < countZero; i++) {
+    arr[i] = 0;
+  }
+
+  for (let i = countZero; i < countOne; i++) {
+    arr[i] = 1;
+  }
+  console.log(arr);
+}
+//ZeroOne(arr);
+
+//!----------------------------------------------------------------
+function OneZero(arr) {
+  let i = 0;
+  let j = arr.length - 1;
+
+  while (i < j) {
+    while (arr[i] == 0) {
+      i++;
+    }
+    while (arr[j] == 1) {
+      j--;
+    }
+    if (i < j) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  console.log(arr);
+}
+OneZero(arr);
