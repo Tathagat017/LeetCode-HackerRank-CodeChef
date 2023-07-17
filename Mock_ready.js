58a10afb436a96c124ede42db039f0fcd1e7da59
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-07-17 18:26:56 +0530

    add two

--- Mock_ready.js
+++ Mock_ready.js
@@ -1,27 +1,35 @@
-// GOOD
-function GoodBadString(str) {
-  let ans = "";
-
-  for (i = 0; i < str.length; i++) {
-    ans = ans + str[i];
-    for (j = i + 1; j < str.length; j++) {
-      if (str[i] == str[j]) {
-        i++;
-      } else {
-        break;
-      }
+//ADD tow
+var addTwoNumbers = function (l1, l2) {
+  let pl1 = l1;
+  let pl2 = l2;
+  let ans = null;
+  let curr = ans;
+  let carry = 0;
+  while (pl1 != null || pl2 != null) {
+    let sum = 0;
+    if (pl1 == null) {
+      sum = pl2.val + carry;
+      pl2 = pl2.next;
+    } else if (pl2 == null) {
+      sum = pl1.val + carry;
+      pl1 = pl1.next;
+    } else {
+      sum = pl1.val + pl2.val + carry;
+      pl1 = pl1.next;
+      pl2 = pl2.next;
+    }
+    carry = Math.floor(sum / 10);
+    sum = sum % 10;
+    if (curr == null) {
+      ans = new ListNode(sum);
+      curr = ans;
+    } else {
+      curr.next = new ListNode(sum);
+      curr = curr.next;
     }
   }
-  console.log(ans);
-}
-function runProgram(input) {
-  input = input.split("\n");
-  let tc = +input[0];
-
-  let line = 1;
-  for (z = 0; z < tc; z++) {
-    let str = input[line];
-    line++;
-    GoodBadString(str);
+  if (carry != 0) {
+    curr.next = new ListNode(curr);
   }
-}
+  return ans;
+};
